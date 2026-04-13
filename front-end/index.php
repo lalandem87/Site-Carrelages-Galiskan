51d99fdbdda12621cb67eca2adeca2d58ada5887
= <=>
2026-04-13 17:41:58 +0200

    Integration du header

---
+++ front-end/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Eray Galiskan Carrelages</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    </head>
+    <body>
+        <header>
+            <div class="logo">
+                <img src="" alt="" />
+            </div>
+            <nav>
+                <ul>
+                    <li><a class="nav-link" href="#à-propos">A propos</a></li>
+                    <li><a class="nav-link" href="#services">Services</a></li>
+                    <li><a class="nav-link" href="">Réalisations</a></li>
+                    <li><a class="nav-link" href="">Avis</a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
