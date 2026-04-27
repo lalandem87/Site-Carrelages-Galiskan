2c32aeddb7dad8cd91793d167bce782d475cf945
= <=>
2026-04-27 15:11:09 +0200

    Carrousel fonctionnel

--- front-end/js/carrousel.js
+++ front-end/js/carrousel.js
@@ -1,10 +1,18 @@
 const arrowLeft = document.getElementById("arrow-left");
 const arrowRight = document.getElementById("arrow-right");
+const track = document.querySelector(".carrousel-track");
 const slides = document.querySelectorAll(".carrousel-item");
-let currentSlide = 1;
+let currentSlide = 0;
 
-function nextImage(index) {
-    currentSlide = index >= slides.length;
+function nextImage() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    track.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
 
-function previousImage() {}
+function previousImage() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    track.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+arrowLeft.addEventListener("click", previousImage);
+arrowRight.addEventListener("click", nextImage);
